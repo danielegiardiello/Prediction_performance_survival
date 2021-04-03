599780b7e69b3a65c57db91b2cb535aefcf6dff5
danielegiardiello <daniele.giardiello1@gmail.com>
2021-04-03 08:28:38 +0200

    Using styler

--- Functions/Rsquared.R
+++ Functions/Rsquared.R
@@ -1,26 +1,28 @@
 # R2
 # Function to calculate R2 and also Royston's D
 # although we suggest time-dependent C and AUC
-Rsq<-function(lp, time, status, ties=TRUE) {
-  pi <- 3.141592653589793    # a user might have overwritten the constant 
+Rsq <- function(lp, time, status, ties = TRUE) {
+  pi <- 3.141592653589793 # a user might have overwritten the constant
   phat <- lp
-  y2 <- Surv(time,status)
+  y2 <- Surv(time, status)
   n <- length(phat)
-  
+
   if (ties && any(duplicated(phat))) {
-    z <- qnorm((1:n - 3/8)/(n + .25))
+    z <- qnorm((1:n - 3 / 8) / (n + .25))
     # per the paper, take the average z over any ties
     index1 <- match(phat, sort(unique(phat)))
-    index2 <- rank(phat, ties='first')
+    index2 <- rank(phat, ties = "first")
     z2 <- tapply(z[index2], index1, mean)
     qhat <- z2[index1]
   }
-  else qhat <- qnorm((rank(phat) -3/8)/(n + .25)) # simple case of no ties
-  
+  else {
+    qhat <- qnorm((rank(phat) - 3 / 8) / (n + .25))
+  } # simple case of no ties
+
   rfit <- coxph(y2 ~ qhat)
   beta <- unname(coef(rfit))
-  D    <- beta* sqrt(8/pi)
-  R2   <- beta^2/ ((pi^2/6) + beta^2)
-  
-  c(D  = D, R2 = R2)  # return vector
+  D <- beta * sqrt(8 / pi)
+  R2 <- beta^2 / ((pi^2 / 6) + beta^2)
+
+  c(D = D, R2 = R2) # return vector
 }
