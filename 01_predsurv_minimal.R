32c55b2d5ea4668d376fea438a07f942aadf307f
danielegiardiello <daniele.giardiello1@gmail.com>
2021-08-29 18:16:18 +0200

    Update 01_predsurv_minimal.R

--- 01_predsurv_minimal.R
+++ 01_predsurv_minimal.R
@@ -59,14 +59,14 @@ gbsg$pgr3 <- rcs3_pgr
 # data sets that are censored at 5 years
 temp <- survSplit(Surv(ryear, rfs) ~ ., 
                   data = rotterdam, 
-                  cut=5,
-                  episode="epoch")
+                  cut = 5,
+                  episode = "epoch")
 rott5 <- subset(temp, epoch==1)  # only the first 5 years
 temp <- survSplit(Surv(ryear, rfs) ~ ., 
                   data = gbsg, 
-                  cut=5,
-                  episode ="epoch")
-gbsg5 <- subset(temp, epoch==1)
+                  cut = 5,
+                  episode = "epoch")
+gbsg5 <- subset(temp, epoch == 1)
 
 # Relevel
 rott5$cnode <- relevel(rotterdam$cnode, "1-3")
